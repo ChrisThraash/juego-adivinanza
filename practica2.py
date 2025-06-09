009a1aa8e90b6747d6084082736b05ccff26e990
Christian <chrishuancabarreto@frba.utn.edu.ar>
2025-06-09 01:32:41 -0300

    practica piedra papel o tijera

---
+++ practica2.py
@@ -0,0 +1,17 @@
+
+nombre1 = input("Ingrese el nombre del primer jugador: ")
+nombre2 = input("Ingrese el nombre del segundo jugador: ")
+
+jugador1 = input("Hola jugador 1!! que eliges? piedra, papel o tijera?: ").strip().lower()
+jugador2 = input("Hola jugador 2!! que eliges? piedra, papel o tijera?: ").strip().lower()
+
+condicion1 = jugador1=="piedra" and jugador2=="tijera"
+condicion2 = jugador1=="papel" and jugador2=="piedra"
+condicion3 = jugador1=="tijeras" and jugador2=="papel"
+
+if jugador1==jugador2:
+    print("Ha sido un empate!!")
+elif condicion1 or condicion2 or condicion3:
+    print("Ha ganado el ",nombre1)
+else:
+    print("Ha ganado ",nombre2)
